346492bfea00b6307663bccd4b1d9f5323a04444
Lukas <105519908+w1nex5372@users.noreply.github.com>
2023-08-13 14:47:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const navToggle = document.getElementById("navToggle");
   const overlay = document.getElementById("overlay");
   const closeOverlay = document.getElementById("closeOverlay");
